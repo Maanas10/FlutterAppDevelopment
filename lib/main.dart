49e2abc897a48db3370b884544d89f6e519a9bef
maanas <maanaspk@gmail.com>
2023-10-28 15:24:37 +0530

    updated

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+void main() {
+
+  runApp(MaterialApp(home:Center(child:Text("Hello",style:TextStyle(fontSize: 50,decoration: TextDecoration.none,color: Colors.blueGrey)))));
+
+}
